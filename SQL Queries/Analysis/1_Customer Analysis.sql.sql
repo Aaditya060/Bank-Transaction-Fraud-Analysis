d2b1cca7d19901a49ee62a7bf06fbe284864159c
Aaditya060 <Aadityajain2006@gmail.com>
2025-10-19 02:45:14 +0530

    Customer & Fraud Analysis Completed

--- SQL Queries/Analysis/1_Customer Analysis.sql.sql
+++ SQL Queries/Analysis/1_Customer Analysis.sql.sql
@@ -1,18 +1,18 @@
 --1. Which age group spends the most on average per transaction?
 
-select age, avg(transaction_amount) as avg_amount from bank_transactions group by age order by avg_amount desc;
+select age, avg(transaction_amount) as "avg_amount" from bank_transactions group by age order by "avg_amount" desc;
 
 --2. Who are the top 10 customers by total spending?
 
-select customer_name, sum(transaction_amount) as total_spent from bank_transactions group by customer_name  order by total_spent desc limit 10;
+select customer_name, sum(transaction_amount) as "total_spent" from bank_transactions group by customer_name  order by "total_spent" desc limit 10;
 
 --3. What is the average transaction amount per gender?
 
-select gender, avg(transaction_amount) as avg_amt from bank_transactions group by gender;
+select gender, avg(transaction_amount) as "avg_amt" from bank_transactions group by gender;
 
 --4. How many customers made transactions in the last month vs. the previous month?
 
 select count(distinct case when transaction_date >= date('2025-09-01') and transaction_date < date('2025-10-01') then customer_id end) 
-as previous_month , count(distinct case when transaction_date >= date('2025-10-01') and transaction_date < date('2025-11-01') then customer_id end) 
-as current_month from bank_transactions;
+as "previous_month" , count(distinct case when transaction_date >= date('2025-10-01') and transaction_date < date('2025-11-01') then customer_id end) 
+as "current_month" from bank_transactions;
 /* date format is in :- year/month/date */
--- SQL Queries/Analysis/2_Fraud Detection.sql
+++ SQL Queries/Analysis/2_Fraud Detection.sql
@@ -1,10 +1,30 @@
 --1. What percentage of transactions are fraudulent?
 
-select (sum(is_fraud)*100/count(*)) as fraud_percentage from bank_transactions;
+select (sum(is_fraud)*100/count(*)) as "fraud percentage" from bank_transactions;
 
 
 --2. Are there specific time periods (hour/day) with higher fraud rates?
 
+select extract(hour from transaction_time) as "Hour of day", count(*) as "total transaction" , sum(is_fraud) as "fraud trans"
+from bank_transactions group by "Hour of day" order by "fraud trans" desc;
+
 --3. Which customers have repeated fraudulent transactions?
 
+select customer_id, customer_name , count(*) as "fraud commition count" ,sum(transaction_amount) as 
+"total fraud amount" from bank_transactions where is_fraud = 1 group by customer_id, customer_name 
+order by "total fraud amount" desc;
+
 --4. Are fraudulent transactions higher for specific transaction amounts?
+
+select case
+when transaction_amount < 500 then '< 500'
+when transaction_amount between 500 and 1000 then '500-1000'
+when transaction_amount between 1000 and 2000 then '1000-2000'
+when transaction_amount between 2000 and 5000 then '2000-5000'
+when transaction_amount between 5000 and 10000 then '5000-10000'
+when transaction_amount between 10000 and 50000 then '10000-50000'
+when transaction_amount between 50000 and 100000 then '50000-100000'
+else '100000+' end as "Amount Range",
+count(*) as "Total Transactions" ,  sum(is_fraud) as "Fraud Transaction",
+round(sum(is_fraud)*100/count(*),2) as "Fraud Percentage" from bank_transactions
+group by "Amount Range" order by "Fraud Percentage" desc;
